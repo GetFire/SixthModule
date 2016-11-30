53cc2ac2f93c95e4fccb0457eb5b3039efa8b16c
Denis Azhymov <azhimov93@gmail.com>
2016-11-30 17:01:07 +0200

    init

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        User user1 = new User(1111, "Ажимов", "Denis", 3200, 2500);
+        User user2 = new User(2222, "Vysotska", "Masha", 3200, 12000);
+        User user3 = new User(2222, "Vysotska", "Tetyana", 3200, 3500);
+        User user4 = new User(3333, "Azhymova", "Tetyana", 3200, 12000);
+        User user5 = user1;
+        User[] users = {user1, user2, user3, user4, user5};
+
+        System.out.println("================== Работаем с массивами =================================");
+        System.out.println();
+
+        int[] arr1 = {1, 2, 3, 4, 53, 6, 7, 8, 9, 10, 53, 2, 0, 1, 22};
+        System.out.println(Arrays.toString(arr1));
+
+        arr1 = ArrayUtils.reverse(arr1);
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(ArrayUtils.findEvenElements(arr1)));
+
+        System.out.println();
+        System.out.println("================== Работаем с Юзерами =======================================");
+        System.out.println();
+
+        UserUtils us = new UserUtils();
+        User[] uniq = us.uniqueUsers(users);
+        System.out.println(Arrays.toString(uniq));
+        System.out.println(Arrays.toString(us.usersWithContitionalBalance(uniq, 2500)));
+        User[] now = users;
+        now[4] = null;
+
+        System.out.println();
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.println();
+
+        System.out.println(Arrays.toString(now));
+        System.out.println(Arrays.toString(us.deleteEmptyUsers(now)));
+
+    }
+
+
+}
--- src/UserUtils.java
+++ src/UserUtils.java
@@ -1,9 +1,8 @@
 
 public class UserUtils {
 
-        public User[] uniqueUsers(User[] users) {
+    public User[] uniqueUsers(User[] users) {
         boolean maska[] = new boolean[users.length];
-//            mask[0]=true;
         int removeCount = 0;
 
         for (int i = 0; i < users.length; i++) {
@@ -11,7 +10,7 @@ public class UserUtils {
                 User tmp = users[i];
 
                 for (int j = i + 1; j < users.length; j++) {
-                    if (tmp.equals(users[j]) ) {
+                    if (tmp.equals(users[j])) {
                         maska[j] = true;
                         removeCount++;
                     }
@@ -29,9 +28,51 @@ public class UserUtils {
 
         return uniq;
     }
-    User[] usersWithContitionalBalance(User[] users, int balance){
 
+    public User[] usersWithContitionalBalance(User[] users, int balance) {
+        User[] needUsers = new User[0];
+        for (int i = 0; i < users.length; i++) {
+            if (users[i].getBalance() == balance) {
+                User[] same = new User[needUsers.length + 1];
+                System.arraycopy(needUsers, 0, same, 0, needUsers.length);
+                needUsers = same;
+                needUsers[needUsers.length - 1] = users[i];
+
+            }
+        }
+        return needUsers;
+    }
+
+    public final User[] paySalaryToUsers(User[] users) {
+        for (int i = 0; i < users.length; i++) {
+            users[i].setBalance(users[i].getBalance() + users[i].getSalary());
+        }
+        return users;
+    }
+
+    public final long[] getUsersId(User[] users) {
+        long[] usID = new long[users.length];
+        for (int i = 0; i < users.length; i++) {
+            usID[i] = users[i].getId();
+        }
+        return usID;
+    }
+    User[] deleteEmptyUsers(User[] users){
+        User[] needUsers = new User[0];
+        for (int i = 0; i < users.length; i++) {
+            if (users[i]!=null){
+                User[] same = new User[needUsers.length + 1];
+                System.arraycopy(needUsers, 0, same, 0, needUsers.length);
+                needUsers = same;
+                needUsers[needUsers.length - 1] = users[i];
+            }
+        }
+        return needUsers;
     }
 
 
-    }
+
+
+
+
+}
