9b9fed5274650e4dbd3e8a5bf114f264b2da4b9d
Denis Azhymov <azhimov93@gmail.com>
2016-11-30 16:00:30 +0200

    init

--- src/User.java
+++ src/User.java
@@ -14,6 +14,31 @@ public class User {
         this.balance = balance;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+        if (salary != user.salary) return false;
+        if (balance != user.balance) return false;
+        if (firstName != null ? !firstName.equals(user.firstName) : user.firstName != null) return false;
+        return lastName != null ? lastName.equals(user.lastName) : user.lastName == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + salary;
+        result = 31 * result + balance;
+        return result;
+    }
+
     public long getId() {
         return id;
     }
@@ -28,6 +53,11 @@ public class User {
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + firstName +" "+  lastName + "}";
     }
 
     public String getLastName() {
--- src/UserUtils.java
+++ src/UserUtils.java
@@ -1,4 +1,37 @@
 
 public class UserUtils {
 
-}
+        public User[] uniqueUsers(User[] users) {
+        boolean maska[] = new boolean[users.length];
+//            mask[0]=true;
+        int removeCount = 0;
+
+        for (int i = 0; i < users.length; i++) {
+            if (!maska[i]) {
+                User tmp = users[i];
+
+                for (int j = i + 1; j < users.length; j++) {
+                    if (tmp.equals(users[j]) ) {
+                        maska[j] = true;
+                        removeCount++;
+                    }
+                }
+            }
+        }
+
+        User[] uniq = new User[users.length - removeCount];
+
+        for (int i = 0, j = 0; i < users.length; i++) {
+            if (!maska[i]) {
+                uniq[j++] = users[i];
+            }
+        }
+
+        return uniq;
+    }
+    User[] usersWithContitionalBalance(User[] users, int balance){
+
+    }
+
+
+    }
